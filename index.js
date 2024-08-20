7adef2279a82759d740ca9296438600b519c2d58
Pola-Gur <pola.gur.il@gmail.com>
2024-08-20 20:44:15 +0300

    added DB

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const sql = require('./app');
 
 
-const PORT = 3001 // || из .env
+const PORT = process.env.PORT || 3001;
 
 app.use(cors())
 
@@ -11,6 +12,16 @@ app.use(cors())
 app.get('/', (req, res) => {
     res.send('Hello from new No Need server!')
 })
+
+app.get('/check-db', async (req, res) => {
+    try {
+        const result = await sql`select version()`;
+        res.send(`Database connected: ${result[0].version}`);
+    } catch (error) {
+        console.error('Error', error);
+        res.status(500).send('Error');
+    }
+});
 
 
 app.listen(PORT, () => {
