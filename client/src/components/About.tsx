529112adce8f978b6c2b92f057ff556e7caff5f9
Pola-Gur <pola.gur.il@gmail.com>
2024-09-01 11:28:57 +0300

    .

--- client/src/components/About.tsx
+++ client/src/components/About.tsx
@@ -4,11 +4,11 @@ const About = () => {
   return (
     <div>
       <h1>About Us</h1>
-      <p>Welcome to our project website!</p>
-      <p>The service was created to connect those who need help with those who are ready to help.
-      In addition, you can create entertaining and educational events to improve your networking and knowledge.
-      Register now and start multiplying good!</p>
-      <img src="../../public/about.jpg" alt="" />
+      <h2>Welcome to our project website!</h2>
+      <h4>The service was created to connect those who need help with those who are ready to help.</h4>
+      <h4>In addition, you can create entertaining and educational events to improve your networking and knowledge.</h4>
+      <h4>Register now and start multiplying good!</h4>
+      <img src="/about.jpg" alt="" />
     </div>
   );
 };
